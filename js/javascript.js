1686f1d244f2c10a36467765117c7d66a686ce20
jerolan <jerome.olvera@gmail.com>
2015-10-09 23:56:11 -0500

    Integracion de sass

---
+++ js/javascript.js
@@ -0,0 +1,12 @@
+/*
+window.addEventListener('load',init);
+function init() {
+	console.log("Hola, yo soy javascript puro");
+}
+*/
+/*
+$(document).ready(function() {
+	console.log("Hola, yo soy jQuery");
+	$('div').html("Hola");
+});
+*/
