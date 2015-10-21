544a1382e3a9e9586044452fe272940b92f3c020
jerome.olvera@gmail.com <jerome.olvera@gmail.com>
2015-10-20 19:27:06 -0500

    Maps & Geo add

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,62 @@
-/*
-window.addEventListener('load',init);
-function init() {
-	console.log("Hola, yo soy javascript puro");
-}
-*/
-/*
+//Funcion que ejecuta todo cuando carga
 $(document).ready(function() {
-	console.log("Hola, yo soy jQuery");
-	$('div').html("Hola");
+	//Variables globales
+	var map;
+	var marker;
+	var me_marker;
+	var geolocalizacion;
+	var center_geo = new google.maps.LatLng(20.967100, -89.623701);
+
+	//El mapa se lanza hasta que carga la pagina
+	google.maps.event.addDomListener(window,'load',dibujarMapa);
+	//Funciones
+	function dibujarMapa() {
+		//Opciones para el mapa
+		var opcionesMapa = {
+			zoom: 15,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		}
+		//Creamos el mapa
+		map = new google.maps.Map(document.querySelector('#map'),opcionesMapa);
+		//API navegacion de HTML5 diferente de maps
+		navigator.geolocation.getCurrentPosition(function(posicion) {
+			//Instanciamos nuestra geolocalizacion
+			geolocalizacion = new google.maps.LatLng(posicion.coords.latitude,posicion.coords.longitude);
+			console.log(center_geo);
+			console.log(geolocalizacion);
+			//Creamos los marcadores
+			//Meta
+			marker = new google.maps.Marker({
+				map: map,
+				position:center_geo,
+				visible: true
+			});
+			//YO
+			me_marker = new google.maps.Marker({
+				map: map,
+				position:geolocalizacion,
+				visible: true
+			});
+			//Centramos el mapa con nuestra localizacion
+			map.setCenter(geolocalizacion);
+			//El geo nos persigue en tiempo real y actualiza la pocicion del mapa
+			navigator.geolocation.watchPosition(actualizarPosicion, function(error) {
+					//Calback para el error, en caso de error
+					console.log(error);
+				}, {
+					//Tiempo de vida de el markador
+					maximumAge: 0
+				});
+		},function(error) { 
+			//Callback para el error 
+			console.log(error);
+		});
+	}
+
+	function actualizarPosicion(posicion) {
+		geolocalizacion = new google.maps.LatLng(posicion.coords.latitude,posicion.coords.longitude);
+		me_marker.setPosition(geolocalizacion);
+		map.setCenter(geolocalizacion);
+	}
+
 });
-*/
--- licencias.md
+++
@@ -1,6 +0,0 @@
-https://flic.kr/p/7MJyBp Ben Hosking 
-2010_2303 - Wood Textures_5
-
-
-https://flic.kr/p/nX1Fb1 PROJuhan Sonin 
-Food is Medicine
